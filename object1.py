038f58765b6814e705981028afbb88b4c04ae3de
senritsuki <senritsuki@gmail.com>
2019-07-24 20:39:35 +0900

    オブジェクト追加

--- object1.py
+++ object1.py
@@ -1,8 +1,8 @@
 import algeol_common as al
 import algeol_primitive as prim
 
-def arch(vf=prim.quarter_cylinder_complement(12), x=1):
-    leaf = al.Node(vf)
+def arch(n=12, x=1):
+    leaf = al.Node(prim.quarter_cylinder_complement(n))
     af1 = al.dotall([
         al.rotate_x90d[1],
         al.rotate_z90d[1],
@@ -14,3 +14,7 @@ def arch(vf=prim.quarter_cylinder_complement(12), x=1):
     edges = [al.Edge(leaf, af) for af in (af1, af2)]
     return al.Node(edges=edges)
 
+def prism_outside(n=12):
+    leaf = al.Node(prim.regular_prism(n))
+    edge = al.Edge(leaf, al.scale((al.R2, al.R2, 1)))
+    return al.Node(edges=[edge])
--- object2.py
+++ object2.py
@@ -1,6 +1,29 @@
 import algeol_common as al
 import algeol_primitive as prim
 
-def rectBorder():
-    pass
+# inner, side, corner は None でもよい
+# inner, side, corner のsizeは 1*1*n を想定している
+def rectBorder(inner, side, corner, dx, dy, ox=0, oy=0):
+    x0 = ox - dx / 2
+    y0 = oy - dy / 2
+    x1 = x0 + dx
+    y1 = y0 + dy
+    edges = []
+    if inner:
+        edges.append(al.Edge(inner, al.dotall([al.translate(ox, oy, 0), al.scale((dx, dy, 1))])))
+    if side:
+        edges += [
+            al.Edge(side, al.dotall([al.translate((ox, y0, 0)), al.scale((dx,  1, 1))])),
+            al.Edge(side, al.dotall([al.translate((ox, y1, 0)), al.scale((dx,  1, 1))])),
+            al.Edge(side, al.dotall([al.translate((x0, oy, 0)), al.scale(( 1, dy, 1))])),
+            al.Edge(side, al.dotall([al.translate((x1, oy, 0)), al.scale(( 1, dy, 1))])),
+        ]
+    if corner:
+        edges += [
+            al.Edge(corner, al.translate((x0, y0, 0))),
+            al.Edge(corner, al.translate((x0, y1, 0))),
+            al.Edge(corner, al.translate((x1, y0, 0))),
+            al.Edge(corner, al.translate((x1, y1, 0))),
+        ]
+    return al.Node(edges=edges)
 
